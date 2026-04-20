d11259303a7005091f97b754a98cefcbb8cbd374
jstzaa <fahrizakurniawan51@gmail.com>
2026-04-20 19:49:41 +0700

    add: routes untuk menampilkan halaman edit

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,20 @@ Route::middleware('auth:admin')->group(function(){
 
     Route::get('/admin/kategori', [Admin\KategoriController::class, 'index'])->name('show.kategori');
     Route::post('/admin/kategori', [Admin\KategoriController::class, 'store'])->name('add.kategori');
+    Route::get('/admin/kategori/{id}', [Admin\KategoriController::class, 'edit'])->name('show.edit.kategori');
     Route::put('/admin/kategori/{id}', [Admin\KategoriController::class, 'update'])->name('update.kategori');
     Route::delete('/admin/kategori/{id}', [Admin\KategoriController::class, 'destroy'])->name('delete.kategori');
 
     Route::get('/admin/daftar-siswa', [Admin\SiswaController::class, 'index'])->name('show.siswa');
     Route::post('/admin/daftar-siswa', [Admin\SiswaController::class, 'store'])->name('add.siswa');
-    Route::put('/admin/daftar-siswa/{id}', [Admin\SiswaController::class, 'update'])->name('update.siswa');
+    Route::get('/admin/daftar-siswa/edit-siswa/{id}', [Admin\SiswaController::class, 'edit'])->name('show.edit.siswa');
+    Route::put('/admin/daftar-siswa/edit-siswa/{id}', [Admin\SiswaController::class, 'update'])->name('update.siswa');
     Route::delete('/admin/daftar-siswa/{id}', [Admin\SiswaController::class, 'destroy'])->name('delete.siswa');
 
     Route::get('/admin/daftar-admin', [Admin\AdminManageController::class, 'index'])->name('show.admin');
     Route::post('/admin/daftar-admin', [Admin\AdminManageController::class, 'store'])->name('add.admin');
-    Route::put('/admin/daftar-admin/{id}', [Admin\AdminManageController::class, 'update'])->name('update.admin');
+    Route::get('/admin/daftar-admin/edit-admin/{id}', [Admin\AdminManageController::class, 'edit'])->name('show.edit.admin');
+    Route::put('/admin/daftar-admin/edit-admin/{id}', [Admin\AdminManageController::class, 'update'])->name('update.admin');
     Route::delete('/admin/daftar-admin/{id}', [Admin\AdminManageController::class, 'destroy'])->name('delete.admin');
 });
 
