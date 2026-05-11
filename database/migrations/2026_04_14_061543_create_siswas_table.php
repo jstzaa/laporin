3f0790fd894543faf06e5e2a7d05fb1c813f8446
jstzaa <fahrizakurniawan51@gmail.com>
2026-05-11 19:24:48 +0700

    refactor(migration): mengubah tipe data kolom nis di tabel siswa

--- database/migrations/2026_04_14_061543_create_siswas_table.php
+++ database/migrations/2026_04_14_061543_create_siswas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('siswas', function (Blueprint $table) {
             $table->id('id_siswa');
             $table->string('nama_siswa', 50);
-            $table->integer('nis')->unique();
+            $table->string('nis', 20)->unique();
             $table->string('password');
             $table->string('kelas', 10);
             $table->timestamps();
